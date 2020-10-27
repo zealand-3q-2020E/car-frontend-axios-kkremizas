9fbc5d3a2bdd34b59f7ceeb537b9723cb8ed8695
Kimon Kremyzas <kimo0016@edu.easj.dk>
2020-10-27 22:20:18 +0100

    Task 2 done

---
+++ src/js/ICar.ts
@@ -0,0 +1,6 @@
+export interface ICar{
+    id: number;
+    vendor: string;
+    model: string;
+    price: number;
+    }
--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,41 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios, {​​ AxiosResponse, AxiosError}​​ from "../../node_modules/axios/index";
+import {ICar} from "./ICar";
+
+let carWebUrl: string = "https://webapicar20190326034339.azurewebsites.net/api/cars";
+let buttonElement:HTMLButtonElement = <HTMLButtonElement> document.getElementById("myButton")
+let ContentElement: HTMLDivElement = <HTMLDivElement> document.getElementById("carsContent");
+
+document.getElementById("myButton").addEventListener("click", GetAllCars)
+
+function GetAllCars(){
+    axios.get<ICar[]>(carWebUrl)
+    .then(function(AxiosResponse){
+        console.log(AxiosResponse);
+        console.log("Status Code: ",AxiosResponse.status);
+        while (ContentElement.firstChild){
+            ContentElement.removeChild(ContentElement.lastChild)
+        }
+        AxiosResponse.data.forEach((car: ICar) => {
+         let newNode:HTMLLIElement = AddLiElement("Vendor: " +car.vendor+", Model: " + car.model +  ", Price: "+car.price);
+            ContentElement.appendChild(newNode);
+        });
+    })
+    .catch(function(error:AxiosError){
+        console.log(error);
+        let errorMessage = "Error Code: "+error.response.status;
+        console.log(errorMessage);
+        while (ContentElement.firstChild){
+            ContentElement.removeChild(ContentElement.lastChild)
+        }
+        let newNode:HTMLLIElement = AddLiElement(errorMessage);
+        ContentElement.appendChild(newNode);
+    })
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
+function AddLiElement(text:string):HTMLLIElement {
+    let newLi:HTMLLIElement = document.createElement('li');
+    let newTextNode:Text = document.createTextNode(text)
+    newLi.appendChild(newTextNode);
+            // list.appendChild(newLi);
+    return newLi;
 }
-let user: Person = { firstName: "John", lastName: "Doe" };
-
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
